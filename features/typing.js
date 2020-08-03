0505f4e7ab19117674c890ddc9350a6cfde3f828
Mason Reichbauer <mreichba@mail.usf.edu>
2020-08-03 14:41:10 -0400

    small changes

--- features/typing.js
+++ features/typing.js
@@ -4,7 +4,7 @@
  */
 const { BotkitConversation } = require("botkit");
 
-module.exports = function(controller) {
+module.exports = function (controller) {
 
     let typing = new BotkitConversation('typing', controller);
 
@@ -12,14 +12,14 @@ module.exports = function(controller) {
     typing.addAction('typing');
 
     // start the typing indicator
-    typing.addMessage({type: 'typing'}, 'typing');
+    typing.addMessage({ type: 'typing' }, 'typing');
     // trigger a gotoThread, which gives us an opportunity to delay the next message
-    typing.addAction('next_thread','typing');
+    typing.addAction('next_thread', 'typing');
 
-    typing.addMessage('typed!','next_thread');
+    typing.addMessage('typed!', 'next_thread');
 
     // use the before handler to delay the next message 
-    typing.before('next_thread',  async () => {
+    typing.before('next_thread', async () => {
         return new Promise((resolve) => {
             // simulate some long running process
             setTimeout(resolve, 3000);
@@ -34,7 +34,7 @@ module.exports = function(controller) {
 
     controller.hears('typing reply', 'message', async (bot, message) => {
 
-        await bot.reply(message, {type: 'typing'});
+        await bot.reply(message, { type: 'typing' });
         setTimeout(async () => {
             // will have to reset context because turn has now ended.
             await bot.changeContext(message.reference);
